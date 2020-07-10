fda714ef629ad3cdd063eb3509759183b7e348d0
chen <2694169035@qq.com>
2020-07-10 11:18:21 +0800

    添加旋转

--- src/views/bofang.vue
+++ src/views/bofang.vue
@@ -2,11 +2,11 @@
     <div>
         <div style="position: fixed; top: 0;left: 0;right: 0;bottom: 0;background-color:#f5f5f5;z-index: -1"></div>
         <Header></Header>
-        <img :src="list.pic_big" class="pic">
+        <img :src="list.pic_big" class="pic" :class="rotate1==true?'route':''">
         <Lrc></Lrc>
         <Action :download="bitrate.file_link"></Action>
         <div class="bofang-container">
-            <audio ref="audio" controls class="bofang" :src="bitrate.show_link"></audio>
+            <audio ref="audio" controls class="bofang" :src="bitrate.show_link" ></audio>
         </div>
     </div>
 </template>
@@ -24,28 +24,35 @@
         components: {
             Header,
             Lrc,
-            Action
+            Action,
         },
         data() {
             return {
                 list: {},
                 bitrate: {},
-                download: ""
+                download: "",
+                rotate1:false
             }
         },
         created: function () {
             console.log(this.$route.params.songId)
             getSingerLrc(this.$route.params.songId).then(res => {
-                console.log(res)
                 this.list = res.songinfo;
                 this.bitrate = res.bitrate
             })
         },
         mounted() {
             this.$refs.audio.addEventListener('timeupdate', () => {
-                console.log(this.$refs.audio.currentTime)
                 this.$store.commit("setCurrentTime", {currentTime: this.$refs.audio.currentTime})
             })
+
+            this.$refs.audio.addEventListener('play', () => {
+                    this.rotate1=true;
+            })
+            this.$refs.audio.addEventListener('pause', () => {
+                    this.rotate1=false  ;
+            })
+
         },
         computed: {
             ...mapState
@@ -64,10 +71,22 @@
         width: 150px;
         height: 150px;
         margin: 100px;
+        border-radius: 50%;
     }
 
     .bofang-container {
         display: flex;
         justify-content: center;
+    }
+    .route {
+        animation:a 10s linear infinite;
+    }
+    @keyframes a {
+        from{
+            transform: rotate(0deg);
+        }
+        to{
+            transform: rotate(360deg);
+        }
     }
 </style>
